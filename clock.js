bee2347c775c5273cf19d0c41b8d8fdc642505ba
lay <1079912423@qq.com>
2018-02-16 16:21:28 +0800

    格式化，改变秒钟格式

--- clock.js
+++ clock.js
@@ -21,76 +21,79 @@ function drawBackground() {
         ctx.fillText(number, x, y);
     });
 
-    for(var i=0;i<60;i++){
-        var rad = 2*Math.PI/60*i;
-        var x=Math.cos(rad)*(r-18);
-        var y=Math.sin(rad)*(r-18);
-        if(i % 5===0){
+    for (var i = 0; i < 60; i++) {
+        var rad = 2 * Math.PI / 60 * i;
+        var x = Math.cos(rad) * (r - 18);
+        var y = Math.sin(rad) * (r - 18);
+        if (i % 5 === 0) {
             ctx.fillStyle = '#000';
         }
-        else{
+        else {
             ctx.fillStyle = '#ccc';
         }
         ctx.beginPath();
-        ctx.arc(x,y,2,0,2*Math.PI,false);
+        ctx.arc(x, y, 2, 0, 2 * Math.PI, false);
         ctx.fill();
     }
 }
-function drawHour(hour,minute){
+function drawHour(hour, minute) {
     ctx.save();
-    var rad = 2*Math.PI/12*hour;
-    var mrad = 2 * Math.PI /12 /60 *minute;
+    var rad = 2 * Math.PI / 12 * hour;
+    var mrad = 2 * Math.PI / 12 / 60 * minute;
     ctx.beginPath();
     ctx.rotate(rad + mrad);
     ctx.lineWidth = 6;
     ctx.lineCap = 'round';
-    ctx.moveTo(0,10);
-    ctx.lineTo(0,-r/2);
+    ctx.moveTo(0, 10);
+    ctx.lineTo(0, -r / 2);
     ctx.stroke();
     ctx.restore();
 }
-function drawMinute(minute){
+function drawMinute(minute) {
     ctx.save();
-    var rad = 2*Math.PI/60*minute;
+    var rad = 2 * Math.PI / 60 * minute;
     ctx.beginPath();
     ctx.rotate(rad);
     ctx.lineWidth = 3;
     ctx.lineCap = 'round';
-    ctx.moveTo(0,10);
-    ctx.lineTo(0,-r+60);
+    ctx.moveTo(0, 10);
+    ctx.lineTo(0, -r + 60);
     ctx.stroke();
     ctx.restore();
 }
-function drawSecond(second){
+function drawSecond(second) {
     ctx.save();
-    var rad = 2*Math.PI/60*second;
+    var rad = 2 * Math.PI / 60 * second;
     ctx.beginPath();
     ctx.rotate(rad);
+    ctx.fillStyle = 'red';
     ctx.lineWidth = 2;
     ctx.lineCap = 'round';
-    ctx.moveTo(0 ,10);
-    ctx.lineTo(0,-r+50);
-    ctx.stroke();
+    ctx.moveTo(-2, 20);
+    ctx.lineTo(2, 20);
+    ctx.lineTo(1, -r + 18);
+    ctx.lineTo(-1, -r + 18);
+    ctx.fill();
     ctx.restore();
 }
-function drawDot(){
-    ctx.beginPath();
+function drawDot() {
+    ctx.beginPath();wcwcwc
     ctx.fillStyle = "#FFF";
-    ctx.arc(0,0,3,0,2*Math.PI,false);
+    ctx.arc(0, 0, 3, 0, 2 * Math.PI, false);
     ctx.fill();
 }
-function draw(){
-    ctx.clearRect(0,0,width,height);
+function draw() {
+    ctx.clearRect(0, 0, width, height);
     var now = new Date();
     var hour = now.getHours();
     var minute = now.getMinutes();
     var second = now.getSeconds();
     drawBackground();
-    drawHour(hour,minute);
+    drawHour(hour, minute);
     drawMinute(minute);
     drawSecond(second);
     drawDot();
     ctx.restore();
 }
 draw();
-setInterval(draw,1000);
+setInterval(draw, 1000);
